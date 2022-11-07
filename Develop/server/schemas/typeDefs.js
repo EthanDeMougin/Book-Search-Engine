3bc68287f653df5ae42dcb2948735f33143aab71
Ethan DeMougin <ethandemougin@gmail.com>
2022-11-07 10:44:59 -0500

    user query and user mutations

--- Develop/server/schemas/typeDefs.js
+++ Develop/server/schemas/typeDefs.js
@@ -2,8 +2,34 @@ const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
   type Query {
-    helloWorld: String
+    me: User
+    users[User]
+    user(username: String!): User
   }
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    savedBooks: Int
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, password: String!, email: String!): Auth
+  }
+  
+  input SaveBookInput {
+    authors: String!
+    description: String!
+    title: String!
+    image: String!
+    link: String!
+  }
+
+  type Auth {
+    token: ID!
+    user: User
 `;
 
 module.exports = typeDefs;
